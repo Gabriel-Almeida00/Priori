98069b80a71d9ad8da1c80ad58ba1b89e27c8d24
gabriel <gaamdal557@gmail.com>
2022-11-19 14:37:34 -0300

    interface generica, usuario e funcao

---
+++ Priori-DADOS/Interfaces/IClienteRepositorio.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Priori_OBJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priori_DADOS.Interfaces
+{
+    internal interface IClienteRepositorio : IRepositorioGenerico<tblClientes>
+    {
+        Task<int> PegarQuantidadeUsuariosRegistrados();
+
+        Task<IdentityResult> CriarCliente(tblClientes cliente, string senha);
+
+        Task IncluirClienteEmFuncao(tblClientes cliente, string funcao);
+
+        Task LogarCliente(tblClientes cliente, bool lembrar);
+
+        Task<tblClientes> PegarClientePeloEmail(string email);
+
+        Task<IList<string>> PegarFuncoesCliente(tblClientes cliente);
+
+        Task AtualizarCliente(tblClientes cliente);
+    }
+}
---
+++ Priori-DADOS/Interfaces/IFuncaoRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using Priori_OBJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priori_DADOS.Interfaces
+{
+    internal interface IFuncaoRepositorio : IRepositorioGenerico<Funcao>
+    {
+        Task AdicionarFuncao(Funcao funcao);
+
+        Task AtualizarFuncao(Funcao funcao);
+
+        IQueryable<Funcao> FiltrarFuncoes(string nomeFuncao);
+
+    }
+}
