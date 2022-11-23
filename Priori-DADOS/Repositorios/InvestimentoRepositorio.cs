abdd43e69293e0c8d370eb3d42b0b79abb66e602
gabriel <gaamdal557@gmail.com>
2022-11-22 22:19:23 -0300

    repositorios completos

---
+++ Priori-DADOS/Repositorios/InvestimentoRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Priori_DADOS.Interfaces;
+using Priori_OBJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Priori_DADOS.Repositorios
+{
+    public class InvestimentoRepositorio : RepositorioGenerico<tblInvestimentos>, IInvestimentoRepositorio
+    {
+        private readonly Contexto _contexto;
+        public InvestimentoRepositorio(Contexto contexto) : base(contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IQueryable<tblInvestimentos> FiltrarInvestimentos(string nomeInvestimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<tblInvestimentos> PegarInvestimentoPeloTipo(string tipo_Investimento)
+        {
+            try
+            {
+                return _contexto.investimentos.Include(c => c.tipo_investimento).Where(c => c.tipo_investimento == tipo_Investimento);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Priori-DADOS/Repositorios/TipoInvestidorRepositorio.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Priori_DADOS.Interfaces;
+using Priori_OBJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Priori_OBJ;
+
+namespace Priori_DADOS.Repositorios
+{
+    public class TipoInvestidorRepositorio : RepositorioGenerico<tbltipoInvestidor>, ITipoInvestidorRepositorio
+    {
+
+        private readonly Contexto _contexto;
+        private readonly UserManager<tbltipoInvestidor> _gerenciadorUsuarios;
+        private readonly RoleManager<tbltipoInvestidor> _gerenciadorFuncoes;
+
+        public TipoInvestidorRepositorio(Contexto contexto, RoleManager<Funcao> gerenciadorFuncoes) : base(contexto)
+        {
+            _contexto = contexto;
+          
+        }
+
+        public async Task AdicionarTipo(tbltipoInvestidor tipo)
+        {
+            try
+            {
+                await _gerenciadorFuncoes.CreateAsync(tipo);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task AtualizarTipo(tbltipoInvestidor tipo)
+        {
+            try
+            {
+                object value = await _gerenciadorUsuarios.UpdateAsync(tipo);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IQueryable<tbltipoInvestidor> FiltrarFuncoes(string nome_categoria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
